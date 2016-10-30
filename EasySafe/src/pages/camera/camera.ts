1e63017261db51d594d99e6d945421c0f28506e7
DESKTOP-20C9TVS\human <human@hoho.com>
2016-10-30 17:47:57 +0900

    add barcode scanner

--- EasySafe/src/pages/camera/camera.ts
+++ EasySafe/src/pages/camera/camera.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { BarcodeScanner } from 'ionic-native';
 /*
   Generated class for the Camera page.
 
@@ -17,6 +17,16 @@ export class CameraPage {
 
   ionViewDidLoad() {
     console.log('Hello Camera Page');
-  }
+  };
 
+  Test(){
+    BarcodeScanner.scan().then((barcodeData) => {
+      alert(barcodeData.text + "/n"+
+      "Format : " + barcodeData.format);
+
+    }, (err) => {
+      console.log(err)
+    });
+
+  }
 }
